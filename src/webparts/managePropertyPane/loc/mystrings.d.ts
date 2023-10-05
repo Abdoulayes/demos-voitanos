ddb763abb5d03f6d9d9e173933f4ebb8da61af80
Abdoulayes <sallcirres@gmail.com>
2023-10-05 14:54:44 +0200

    Init

---
+++ src/webparts/managePropertyPane/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IManagePropertyPaneWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ManagePropertyPaneWebPartStrings' {
+  const strings: IManagePropertyPaneWebPartStrings;
+  export = strings;
+}
